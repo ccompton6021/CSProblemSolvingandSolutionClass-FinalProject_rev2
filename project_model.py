2a10f01a865d004023ce7535815c5e3be6431094
Joseph Companion <jcompanion4914@floridapoly.edu>
2023-12-10 16:02:41 -0500

    Update project_model.py

--- project_model.py
+++ project_model.py
@@ -15,13 +15,13 @@ class Model:
     def load_file(self, file_path):
         # Load audio file
         self.filename = file_path
-        if file_path.lower().endswith(('.wav', '.mp3', '.aac')):
-            if file_path.lower().endswith(('.mp3', '.aac')):
+        if file_path.lower().endswith(('.wav', '.mp3', '.aac','.m4a')):
+            if file_path.lower().endswith(('.mp3', '.aac','.m4a')):
                 # convert files to .wav
                 self.convert_to_wav(file_path)
 
             # Load audio using librosa for flexible format support
-            self.audio_data, self.sample_rate = librosa.load(file_path, sr=None, mono=False)
+            self.audio_data, self.sample_rate = librosa.load(file_path, sr=None, mono=False, res_type='kaiser_fast')
 
             # Handle metadata
             if len(self.audio_data.shape) > 1 and self.audio_data.shape[0] > 1:
@@ -36,6 +36,6 @@ class Model:
 
     def remove_metadata(self, file_path):
         audiofile = eyed3.load(file_path)
-        if audiofile.tag:
+        if audiofile is not None and audiofile.tag:
             audiofile.tag.frame_set.clear()
             audiofile.tag.save()
